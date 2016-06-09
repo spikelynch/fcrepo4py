21271db07ac856d482713d048d217e58f19788d5
Mike Lynch <mike.lynch@iinet.net.au>
2016-06-09 13:57:06 +1000

    Added sample config file for tests

--- test_001_connect.py
+++ test_001_connect.py
@@ -5,6 +5,7 @@ class TestConnect(unittest.TestCase):
         
     def test_connect(self):
         repo = fcrepo4.Repository()
+        repo.set_user('fedoraAdmin')
         self.assertIsNotNone(repo)
         res = repo.get(repo.path2uri('/'))
         self.assertIsNotNone(res)
